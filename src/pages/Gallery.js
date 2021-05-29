6eaffc6c50b9ecbc352c59bd098bebcb6419235e
martinigonsalves <32884454+martinigonsalves@users.noreply.github.com>
2021-05-29 21:42:21 +0200

    created galleryitem

--- src/pages/Gallery.js
+++ src/pages/Gallery.js
@@ -1,9 +1,20 @@
 import React from "react";
+import { GalleryList } from "../helpers/GalleryList";
+import GalleryItem from "../components/GalleryItem";
 
 function Gallery() {
   return (
     <div className="gallery">
       <h1 className="galleryTitle">Gallerij</h1>
+      <div className="galleryList">
+        {GalleryList.map((foto, key) => {
+          return (
+            <div>
+              <GalleryItem key={key} name={foto.name} />
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
