1153ff8d0827ccf5619201dabb7b278968edf73a
martinigonsalves <32884454+martinigonsalves@users.noreply.github.com>
2021-05-29 15:22:06 +0200

    adding links

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import "./App.css";
 import Navbar from "./components/Navbar";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <div className="App">
       <Router>
         <Navbar />
+        <Switch>
+          <Route path="/" exact component={Home} />
+        </Switch>
       </Router>
     </div>
   );
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,18 @@
 import React from "react";
 import Logo from "../assets/pizzaLogo.png";
+import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
     <div className="navbar">
       <div className="leftside"></div>
-      <img src={Logo} />
-      <div className="rightside"></div>
+      <img src={Logo} alt="logo" />
+      <div className="rightside">
+        <Link to="/">Home</Link>
+        <Link to="/">About us</Link>
+        <Link to="/">Gallery</Link>
+        <Link to="/">Contact</Link>
+      </div>
     </div>
   );
 }
