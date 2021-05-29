dd04c1a8d88651c7b49aad345ec17aa915d3c3cb
martinigonsalves <32884454+martinigonsalves@users.noreply.github.com>
2021-05-29 15:13:31 +0200

    adding router dom

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import "./App.css";
+import Navbar from "./components/Navbar";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Router>
+        <Navbar />
+      </Router>
+    </div>
+  );
 }
 
 export default App;
