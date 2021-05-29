16e8884d6d588335f85d7737f5faa47854d231a5
martinigonsalves <32884454+martinigonsalves@users.noreply.github.com>
2021-05-29 17:40:08 +0200

    create home page and home css

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,20 @@
 import React from "react";
-
+import { Link } from "react-router-dom";
+import Afbeelding from "../assets/afbeelding.jpeg";
+import "../styles/Home.css";
 function Home() {
   return (
-    <div>
-      <h1>Hinzen bestratingen</h1>
+    <div className="home">
+      <div
+        className="headerContainer"
+        style={{ backgroundImage: `url(${Afbeelding})` }}
+      >
+        <h1>Hinzen bestratingen</h1>
+        <p>Welkom op onze website</p>
+        <Link to="/gallery">
+          <button>Bekijk onze werken</button>
+        </Link>
+      </div>
     </div>
   );
 }
