fce8ed718bc605b69e5f89a56701bdab6d67d754
martinigonsalves <32884454+martinigonsalves@users.noreply.github.com>
2021-05-29 14:49:16 +0200

    first commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Navbar() {
+  return (
+    <div className="navbar">
+      <div className="leftside"></div>
+      <div className="rightside"></div>
+    </div>
+  );
+}
+
+export default Navbar;
