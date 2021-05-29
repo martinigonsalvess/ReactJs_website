3129bf9e22a58d7801538cc3341e9629d5c01041
martinigonsalves <32884454+martinigonsalves@users.noreply.github.com>
2021-05-29 20:33:37 +0200

    creating galleryList and import fotos

---
+++ src/helpers/GalleryList.js
@@ -0,0 +1,33 @@
+import foto1 from "../assets/foto1.jpg";
+import foto2 from "../assets/foto2.jpg";
+import foto3 from "../assets/foto3.jpg";
+import foto4 from "../assets/foto4.jpg";
+import foto5 from "../assets/foto5.jpg";
+import foto6 from "../assets/foto6.jpg";
+
+export const GalleryList = [
+  {
+    name: "foto 1",
+    image: foto1,
+  },
+  {
+    name: "foto 2",
+    image: foto2,
+  },
+  {
+    name: "foto 3",
+    image: foto3,
+  },
+  {
+    name: "foto 4",
+    image: foto4,
+  },
+  {
+    name: "foto 5",
+    image: foto5,
+  },
+  {
+    name: "foto 6",
+    image: foto6,
+  },
+];
--- src/pages/Gallery.js
+++ src/pages/Gallery.js
@@ -1,7 +1,11 @@
 import React from "react";
 
 function Gallery() {
-  return <div className="gallery"></div>;
+  return (
+    <div className="gallery">
+      <h1 className="galleryTitle">Gallerij</h1>
+    </div>
+  );
 }
 
 export default Gallery;
