8a62857f62f4615cf1e58f6ffa9d0c2b59849ff5
martinigonsalves <32884454+martinigonsalves@users.noreply.github.com>
2021-05-29 17:11:53 +0200

    react hooks useState to show menu left or not

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,19 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import Logo from "../assets/pizzaLogo.png";
 import { Link } from "react-router-dom";
 import "../styles/Navbar.css";
+import ReorderIcon from "@material-ui/icons/Reorder";
 
 function Navbar() {
   return (
     <div className="navbar">
       <div className="leftSide">
         <img src={Logo} />
+        <div className="hiddenLinks">
+          <Link to="/">Home</Link>
+          <Link to="/gallery">Gallery</Link>
+          <Link to="/about">About us</Link>
+          <Link to="/contact">Contact</Link>
+        </div>
       </div>
       <div className="rightSide">
         <Link to="/">Home</Link>
         <Link to="/gallery">Gallery</Link>
         <Link to="/about">About us</Link>
         <Link to="/contact">Contact</Link>
+        <button>
+          <ReorderIcon />
+        </button>
       </div>
     </div>
   );
