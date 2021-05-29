adc4326995733c8e3521759194520e35eafb22f9
martinigonsalves <32884454+martinigonsalves@users.noreply.github.com>
2021-05-29 15:34:50 +0200

    added css styles

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,17 +1,19 @@
 import React from "react";
 import Logo from "../assets/pizzaLogo.png";
 import { Link } from "react-router-dom";
+import "../styles/Navbar.css";
 
 function Navbar() {
   return (
     <div className="navbar">
-      <div className="leftside"></div>
-      <img src={Logo} alt="logo" />
-      <div className="rightside">
+      <div className="leftSide">
+        <img src={Logo} />
+      </div>
+      <div className="rightSide">
         <Link to="/">Home</Link>
-        <Link to="/">About us</Link>
-        <Link to="/">Gallery</Link>
-        <Link to="/">Contact</Link>
+        <Link to="/gallery">Gallery</Link>
+        <Link to="/about">About us</Link>
+        <Link to="/contact">Contact</Link>
       </div>
     </div>
   );
